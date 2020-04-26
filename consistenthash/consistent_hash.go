5d4dcfe893f5d6f94bc0fc642bd24a4b8527dd76
Marcos <marcos.sanchez@etoshi.io>
2020-04-26 22:10:24 +0200

    feat: add consistent map to map keys with nodes

--- consistenthash/consistent_hash.go
+++ consistenthash/consistent_hash.go
@@ -1,1 +1,68 @@
 package consistenthash
+
+import (
+	"crypto/md5"
+	"encoding/binary"
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+type HasherFunc func([]byte) int
+
+type Hasher interface {
+	Hash() HasherFunc
+}
+
+type ConsistentHash struct {
+	hasher   HasherFunc
+	replicas int
+	hashmap  map[int]string
+	keys     []int
+}
+
+func New(replicas int, fn HasherFunc) *ConsistentHash {
+	return &ConsistentHash{
+		hasher:   fn,
+		replicas: replicas,
+		hashmap:  make(map[int]string),
+	}
+}
+
+func (ch *ConsistentHash) Hash(data []byte) int {
+	return ch.hasher(data)
+}
+
+func (ch *ConsistentHash) Add(keys ...string) *ConsistentHash {
+	for _, key := range keys {
+		for i := 1; i <= ch.replicas; i++ {
+			hash := ch.Hash([]byte(strconv.Itoa(i) + key))
+			ch.keys = append(ch.keys, hash)
+			ch.hashmap[hash] = key
+		}
+	}
+	sort.Ints(ch.keys)
+	return ch
+}
+
+func (ch *ConsistentHash) Get(key string) string {
+	hash := ch.hasher([]byte(key))
+	klen := len(ch.keys)
+	ki := sort.Search(klen, func(i int) bool {
+		return ch.keys[i] >= hash
+	})
+	if ki == klen {
+		ki = 0
+	}
+	return ch.hashmap[ch.keys[ki]]
+}
+
+func HashCRC32(data []byte) int {
+	return int(crc32.ChecksumIEEE(data))
+}
+
+func HashMD5(data []byte) int {
+	hashed := md5.Sum(data)
+	slice := hashed[:]
+	return int(binary.LittleEndian.Uint32(slice))
+}
--- consistenthash/consistent_hash_test.go
+++ consistenthash/consistent_hash_test.go
@@ -1,1 +1,84 @@
 package consistenthash
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+type testS struct {
+	key  string
+	node string
+}
+
+func testHasher(data []byte) int {
+	i, err := strconv.Atoi(string(data))
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func testConsistentMap(t *testing.T, c *ConsistentHash, tests []testS) {
+	t.Helper()
+
+	for _, test := range tests {
+		if c.Get(test.key) != test.node {
+			t.Errorf("unexpected node for key %s. want %s, have %s",
+				test.key, test.node, c.Get(test.key))
+		}
+	}
+}
+
+func TestConsistentHash_Add(t *testing.T) {
+	keys := []string{"1", "2", "3"} // [11, 21, 12, 22, 13, 23] -> // [11, 12, 13, 21, 21, 22, 23]
+	cmap := New(2, testHasher)
+	cmap.Add(keys...)
+	tests := []testS{
+		{"3", "1"},
+		{"12", "2"},
+		{"13", "3"},
+		{"10", "1"},
+		{"21", "1"},
+		{"22", "2"},
+		{"23", "3"},
+		{"24", "1"},
+	}
+
+	testConsistentMap(t, cmap, tests)
+
+	cmap.Add("8") // [11, 21, 12, 22, 13, 23, 18, 81] -> // [11, 12, 13, 18, 21, 21, 22, 23, 81]
+	tests = tests[:len(tests)-1]
+	tests = append(tests, testS{"24", "8"})
+	tests = append(tests, testS{"25", "8"})
+
+	testConsistentMap(t, cmap, tests)
+
+}
+
+func BenchmarkGet8_MD5(b *testing.B)     { benchmarkGet(b, 8, HashMD5) }
+func BenchmarkGet32_MD5(b *testing.B)    { benchmarkGet(b, 32, HashMD5) }
+func BenchmarkGet128_MD5(b *testing.B)   { benchmarkGet(b, 128, HashMD5) }
+func BenchmarkGet512_MD5(b *testing.B)   { benchmarkGet(b, 512, HashMD5) }
+func BenchmarkGet8_CRC32(b *testing.B)   { benchmarkGet(b, 8, HashCRC32) }
+func BenchmarkGet32_CRC32(b *testing.B)  { benchmarkGet(b, 32, HashCRC32) }
+func BenchmarkGet128_CRC32(b *testing.B) { benchmarkGet(b, 128, HashCRC32) }
+func BenchmarkGet512_CRC32(b *testing.B) { benchmarkGet(b, 512, HashCRC32) }
+
+func benchmarkGet(b *testing.B, shards int, fn HasherFunc) {
+
+	hash := New(50, fn)
+
+	var buckets []string
+	for i := 0; i < shards; i++ {
+		buckets = append(buckets, fmt.Sprintf("shard-%d", i))
+	}
+
+	hash.Add(buckets...)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		hash.Get(buckets[i&(shards-1)])
+	}
+}
